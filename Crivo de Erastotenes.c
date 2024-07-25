09d330fddd01b71c9ec784c1438563557cf70906
Matheus <137547780+Mathayuz@users.noreply.github.com>
2024-07-25 12:54:06 -0300

    Update Crivo de Erastotenes.c

--- Crivo de Erastotenes.c
+++ Crivo de Erastotenes.c
@@ -1,51 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
-#include <stdbool.h>
 #include <math.h>
+#include <stdint.h>
+#include <string.h>
 
-#define MAX_NUM 6400
+void crivo_de_eratostenes(uint32_t *primo, int n) {
+    int fim = (int)sqrt(n);
 
-// Function to set the bit at position 'pos' in the bit array
-void setBit(int *bitArray, int pos) {
-    int index = pos / 32; // Divide by 32 to get the index of the int
-    int bitPos = pos % 32; // Get the bit position within the int
+    // Inicializa todos os bits como 1 (números primos)
+    for (int i = 0; i <= n / 32; ++i) {
+        primo[i] = 0xFFFFFFFF;
+    }
 
-    bitArray[index] |= (1 << bitPos); // Set the bit at the given position
-}
+    // Define 0 e 1 como não primos
+    primo[0] &= ~3;
 
-// Function to check if the bit at position 'pos' is set in the bit array
-bool isBitSet(int *bitArray, int pos) {
-    int index = pos / 32; // Divide by 32 to get the index of the int
-    int bitPos = pos % 32; // Get the bit position within the int
-
-    return (bitArray[index] & (1 << bitPos)) != 0; // Check if the bit is set
-}
-
-// Function to create the prime number bit array
-void createBitArray(int *bitArray, int n) {
-    int i, j;
-    for (i = 2; i <= n; i++) {
-        if (!isBitSet(bitArray, i)) {
-            for (j = i + i; j <= n; j += i) {
-                setBit(bitArray, j);
+    for (int p = 2; p <= fim; ++p) {
+        if (primo[p / 32] & (1 << (p % 32))) {
+            for (int k = p * p; k <= n; k += p) {
+                primo[k / 32] &= ~(1 << (k % 32));
             }
         }
     }
 }
 
+int eh_primo(uint32_t *primo, int num) {
+    if (num < 2) {
+        return 0;
+    }
+
+    return (primo[num / 32] & (1 << (num % 32))) != 0;
+}
+
 int main() {
-    int bitArray[(MAX_NUM + 32) / 32] = {0}; // Bit array to store prime numbers
+    int n = 6400;
+    uint32_t primo[200]; // 6400 / 32 = 200
+    memset(primo, 0, sizeof(primo)); // Inicializa todos os bytes do array com 0
+
+    crivo_de_eratostenes(primo, n);
+
+    // Exemplos de consulta
     int num;
-
-    createBitArray(bitArray, MAX_NUM);
-
-    printf("Digite um número: ");
+    printf("Digite um numero para verificar se eh primo (entre 2 e %d): ", n);
     scanf("%d", &num);
 
-    if (num >= 2 && num <= MAX_NUM && !isBitSet(bitArray, num)) {
-        printf("O número %d é primo\n", num);
+    if (eh_primo(primo, num)) {
+        printf("%d eh um numero primo.\n", num);
     } else {
-        printf("O número %d não é primo\n", num);
+        printf("%d nao eh um numero primo.\n", num);
     }
 
     return 0;
